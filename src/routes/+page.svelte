c305d31b2ac8a58c6aaaf7f5bc607ba617d03cae
Hugo <hugokorteapple@gmail.com>
2023-06-06 20:21:06 +0200

    removed snap stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
-<div class="px-4 md:px-[25vw] pt-[10vh] flex flex-col gap-[50vh] snap-y snap-mandatory">
-    <section class="snap-start">
+<div class="px-4 md:px-[25vw] pt-[10vh] flex flex-col gap-[50vh]">
+    <section>
         <h1 class="text-7xl font-semibold leading-loose" id="home">Hugo Korte</h1>
         <p class="text-xl max-w-[50ch]">Passionate software engineer with a creative mindset, dedicated to crafting innovative solutions that blend functionality, usability, and elegant design.</p>
     </section>
-    <section class="snap-start">
+    <section>
         <h2 class="text-end text-6xl font-semibold leading-loose" id="about">About</h2>
         <p class="text-xl max-w-[50ch">
             I'm Hugo Korte aka Hugos, a passionate software engineer and second-year bachelor student in computer science. My journey began with Java, where I discovered my love for programming. Since then, I have been continuously expanding my skills and knowledge.
@@ -11,12 +11,12 @@
             When I'm not programming, you can find me sipping on a cup of coffee, engaging in friendly gaming sessions, or exploring new technologies and frameworks. I am always eager to learn and adapt to the ever-evolving landscape of software engineering.
             I am currently seeking opportunities to apply my skills and contribute to exciting projects. If you are looking for a motivated and adaptable software engineer, let's connect and discuss how we can work together to bring innovative ideas to life.
         </p>
-    <section class="snap-start">
+    <section>
         <h2 class="text-6xl font-semibold leading-loose" id="work">Work</h2>
 
     <section>
 
-    <section class="snap-start">
+    <section>
         <h2 class="text-end text-6xl font-semibold leading-loose" id="skillset">Skillset</h2>
 
     </section>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 <svelte:window bind:scrollY />
 
-<header class="sticky {scrollY > 50 ? "h-20 px-[5vw] md:px-[20vw] bg-white text-black" : "h-36 px-[5vw] bg-black text-white"} transition-[background,height,padding,color] duration-[350ms] top-0 flex items-center justify-between">
+<header class="sticky {scrollY > 50 ? "h-20 px-[5vw] md:px-[20vw] bg-white text-black" : "h-36 px-[5vw] bg-black text-white"} transition-[background,height,padding,color] duration-300 top-0 flex items-center justify-between">
     <a href="#home" class="text-2xl font-extrabold">Hugo Korte</a>
     <nav class="font-bold flex items center gap-4">
         <a href="#about">About</a>
