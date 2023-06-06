f3ec64a9bb479d29501a4aaefb214780fa40eb2b
Hugo <hugokorteapple@gmail.com>
2023-06-06 20:08:05 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import "../app.css";    
+    
+	import Footer from "./Footer.svelte";
+    import Main from "./Main.svelte";
+    import Header from "./Header.svelte";
+</script>
+
+<Header />
+<Main><slot/></Main>
+<Footer />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div class="px-4 md:px-[25vw] pt-[10vh] flex flex-col gap-[50vh] snap-y snap-mandatory">
+    <section class="snap-start">
+        <h1 class="text-7xl font-semibold leading-loose" id="home">Hugo Korte</h1>
+        <p class="text-xl max-w-[50ch]">Passionate software engineer with a creative mindset, dedicated to crafting innovative solutions that blend functionality, usability, and elegant design.</p>
+    </section>
+    <section class="snap-start">
+        <h2 class="text-end text-6xl font-semibold leading-loose" id="about">About</h2>
+        <p class="text-xl max-w-[50ch">
+            I'm Hugo Korte aka Hugos, a passionate software engineer and second-year bachelor student in computer science. My journey began with Java, where I discovered my love for programming. Since then, I have been continuously expanding my skills and knowledge.
+            Frontend development is my primary focus, and I have hands-on experience with SvelteKit. I enjoy creating seamless user experiences and crafting elegant, responsive interfaces. I believe that good design, coupled with efficient code, can truly make a difference in the digital world.
+            When I'm not programming, you can find me sipping on a cup of coffee, engaging in friendly gaming sessions, or exploring new technologies and frameworks. I am always eager to learn and adapt to the ever-evolving landscape of software engineering.
+            I am currently seeking opportunities to apply my skills and contribute to exciting projects. If you are looking for a motivated and adaptable software engineer, let's connect and discuss how we can work together to bring innovative ideas to life.
+        </p>
+    <section class="snap-start">
+        <h2 class="text-6xl font-semibold leading-loose" id="work">Work</h2>
+
+    <section>
+
+    <section class="snap-start">
+        <h2 class="text-end text-6xl font-semibold leading-loose" id="skillset">Skillset</h2>
+
+    </section>
+</div>
+
+
+
+
+
