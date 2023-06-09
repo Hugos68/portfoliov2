40951e99f204589557e0cfcb99764f7caea817ca
Hugo <hugokorteapple@gmail.com>
2023-06-09 16:14:28 +0200

    converted to skeleton for theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,13 @@
-<div class="px-4 md:px-[25vw] pt-[10vh] flex flex-col gap-[50vh]">
-    <section>
-        <h1 class="text-7xl font-semibold leading-loose" id="home">Hugo Korte</h1>
-        <p class="text-xl max-w-[50ch]">Passionate software engineer with a creative mindset, dedicated to crafting innovative solutions that blend functionality, usability, and elegant design.</p>
-    </section>
-    <section>
-        <h2 class="text-end text-6xl font-semibold leading-loose" id="about">About</h2>
-        <p class="text-xl max-w-[50ch">
-            I'm Hugo Korte aka Hugos, a passionate software engineer and second-year bachelor student in computer science. My journey began with Java, where I discovered my love for programming. Since then, I have been continuously expanding my skills and knowledge.
-            Frontend development is my primary focus, and I have hands-on experience with SvelteKit. I enjoy creating seamless user experiences and crafting elegant, responsive interfaces. I believe that good design, coupled with efficient code, can truly make a difference in the digital world.
-            When I'm not programming, you can find me sipping on a cup of coffee, engaging in friendly gaming sessions, or exploring new technologies and frameworks. I am always eager to learn and adapt to the ever-evolving landscape of software engineering.
-            I am currently seeking opportunities to apply my skills and contribute to exciting projects. If you are looking for a motivated and adaptable software engineer, let's connect and discuss how we can work together to bring innovative ideas to life.
-        </p>
-    <section>
-        <h2 class="text-6xl font-semibold leading-loose" id="work">Work</h2>
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-    <section>
-
-    <section>
-        <h2 class="text-end text-6xl font-semibold leading-loose" id="skillset">Skillset</h2>
-
-    </section>
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Let's get cracking bones!</h1>
+		<p>Start by exploring:</p>
+		<ul>
+			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
+			<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
+			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
+		</ul>
+	</div>
 </div>
-
-
-
-
-
