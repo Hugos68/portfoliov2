baa043a63bc56d64331d7a3ed10bb3637facbd65
Hugo <hugokorteapple@gmail.com>
2023-06-09 17:11:11 +0200

    added some before classes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang='ts'>
-	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-modern.css';
-	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '../theme.postcss';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	
+	import Footer from './Footer.svelte';
+	import Header from './Header.svelte';
+	import Main from './Main.svelte';
 </script>
 
-<slot />
+<Header />
+<Main><slot /></Main>
+<Footer />
+
---
+++ src/routes/Header.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    type NavItem = { label: string; href: string; }
+
+    const navItems: NavItem[] = [
+        {
+            label: 'Hugo Korte',
+            href: '/'
+        },
+        {
+            label: 'About',
+            href: '/about'
+        },
+        {
+            label: 'Work',
+            href: '/work'
+        },
+        {
+            label: 'Competence',
+            href: '/competence'
+        }
+    ];
+</script>
+
+<header class="h-20 bg-gradient-to-r from-primary-500/50  to-secondary-500/25 m-2 rounded-token px-10">
+    <nav class="h-full flex [&>*:nth-child(2)]:ml-auto 0 items-center gap-5 font-semibold text-lg">
+        {#each navItems as {label, href}}
+            <a href={href} class="px-3 py-2 hover:bg-surface-100/25 rounded-token transition-[background]">{label}</a>
+        {/each}
+    </nav>
+</header>
